ff81cabd6e2a538de9ffb0e5c3b41c3c0c46a8f7
Shivam Mishra <shivam9878522087@gmail.com>
2026-02-07 14:24:36 +0530

    feat: add zod validation for auth login route

--- src/routes/v1/auth-routes.ts
+++ src/routes/v1/auth-routes.ts
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
 import { signIn, signUp } from '../../controllers/auth-controller';
 import validate from '../../middlewares/validate-middleware';
-import { userSignupSchema } from '../../schemas/auth-schema';
+import { userSigninSchema, userSignupSchema } from '../../schemas/auth-schema';
 
 const router: Router = express.Router();
 
 router.post('/signup', validate(userSignupSchema), signUp);
 
-router.post('/login', signIn);
+router.post('/login', validate(userSigninSchema), signIn);
 
 export default router;
--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -5,3 +5,8 @@ export const userSignupSchema = z.object({
     password: z.string().min(8, 'Password must be at least 8 characters'),
     role: z.enum(['host', 'user']),
 });
+
+export const userSigninSchema = z.object({
+    email: z.string().email('Invalid email address').min(1, 'Email is required'),
+    password: z.string().min(8, 'Password must be at least 8 characters'),
+});
